19726cb12e3ac9cbec7a397cbe22852503100752
Fabbiewoods <fabbiewoods@gmail.com>
2023-10-05 03:09:01 -0700

    oh puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ *main-this is the main function
+ *
+ *Return:0 on success, or a non-zero value on failure
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
