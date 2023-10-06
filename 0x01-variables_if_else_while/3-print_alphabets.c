a9c7351a37e9d9e9a1a71e967e2b7f8ace4afbaa
Fabbiewoods <fabbiewoods@gmail.com>
2023-10-06 16:43:50 -0700

    upper and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point for the program.
+ *
+ * Return: this can be 0 on success.
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
