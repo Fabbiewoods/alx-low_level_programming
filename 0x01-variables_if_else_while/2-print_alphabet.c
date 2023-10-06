122a63c0edbf9bd4b00983ad85816264b9800fdc
Fabbiewoods <fabbiewoods@gmail.com>
2023-10-06 16:25:12 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is tge entry point of the program.
+ *
+ * Return: this can return 0 on success.
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'a';
+	while
+		(i <= 'z') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
+
