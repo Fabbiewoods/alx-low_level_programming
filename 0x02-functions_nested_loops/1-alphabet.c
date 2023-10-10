93dee1da6f16310e7f253e6e6327145b2dbfe37b
Fabbiewoods <fabbiewoods@gmail.com>
2023-10-10 14:40:00 -0700

    suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this function is to print alphabets in lower case.
+ *
+ * Return: this returns 0
+ */
+
+void print_alphabet(void)
+{
+	char u;
+
+	for (u = 'a' ; u <= 'z' ; u++)
+		_putchar(u);
+	_putchar('\n');
+}
