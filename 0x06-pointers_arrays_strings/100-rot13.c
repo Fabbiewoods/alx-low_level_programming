5e76fa945c4e3ba69382277a314af1f50ff7520b
Fabbiewoods <fabbiewoods@gmail.com>
2023-10-19 22:16:36 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,16 +11,16 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWZYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 32; j++)
+		for (j = 0; c[j] != '\0'; j++)
 		{
-			if (s[i] == data1[j])
+			if (s[i] == c[j])
 			{
-				s[i] = datarot[j];
+				s[i] = d[j];
 				break;
 			}
 		}
