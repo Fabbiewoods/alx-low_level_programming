5c78cf28b298dfb572bb71c905130691107e22c5
Fabbiewoods <fabbiewoods@gmail.com>
2023-11-03 10:10:19 -0700

    seward

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,15 @@
 char *create_array(unsigned int size, char c)
 
 {
-	char *str;
 	unsigned int i;
+	char *str;
 
 	if (size == 0)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
 
- 	if (str == NULL)
- 		return (NULL);
+	if (str == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
